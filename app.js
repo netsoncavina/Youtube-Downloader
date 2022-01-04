84ec2918dd433813a09ae3fe19cd604b1253208e
Netson <netsoncavina@gmail.com>
2022-01-04 16:26:59 -0300

    Integrated front and backend

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const ytdl = require("ytdl-core");
 const app = express();
@@ -5,13 +6,19 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   const { url } = req.query;
+
   ytdl.getInfo(url).then((info) => {
     let name = info.videoDetails.title;
     let thumbnail = info.videoDetails.thumbnail.thumbnails[4].url;
-    res.render("index.ejs", { name, thumbnail });
+
     // res.header("Content-Disposition", `attachment; filename="${name}.mp4"`);
+
+    res.render("index.ejs", { name, thumbnail });
+
+    mp4 = `./${name}.mp4`;
+    ytdl(url).pipe(fs.createWriteStream(mp4));
     // return ytdl(url).pipe(res);
   });
 });
 
-app.listen(3000);
+app.listen(5000, () => console.log("Server started on port 5000"));
