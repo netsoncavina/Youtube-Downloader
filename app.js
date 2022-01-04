db79e530b5f1a1599c9884c2878fda1baa387c39
Netson <netsoncavina@gmail.com>
2022-01-04 11:25:52 -0300

    Added front end

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const ytdl = require("ytdl-core");
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   const { url } = req.query;
-  //   const date = Date.now();
-  let name;
   ytdl.getInfo(url).then((info) => {
-    let id = info.videoDetails.title;
-    // console.log(id);
-    res.header("Content-Disposition", `attachment; filename="${id}.mp4"`);
-    return ytdl(url).pipe(res);
+    let name = info.videoDetails.title;
+    let thumbnail = info.videoDetails.thumbnail.thumbnails[4].url;
+    res.render("index.ejs", { name, thumbnail });
+    // res.header("Content-Disposition", `attachment; filename="${name}.mp4"`);
+    // return ytdl(url).pipe(res);
   });
 });
 
